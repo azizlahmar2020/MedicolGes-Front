1a51a76af4f6458132587034e66395dd93bb1808
Mohamed Aziz Lahmar <92262308+azizlahmar2020@users.noreply.github.com>
2024-03-14 12:21:32 +0100

    Revert "Merge branch 'main' into Mohamed_aziz_lahmar"
    This reverts commit 13999d766a3c177ed58d38b06786e43b3d7fb0b3, reversing
    changes made to 540e901a6d5e621914360c854b2a36540902e7df.

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-// src/services/api.js
-
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000', // Update with your backend API URL
-});
-
-export default api;
